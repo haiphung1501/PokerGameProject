fe4a71acaa0e7af4fe52d27491a3637e91f9d70c
haiphung1501 <80373276+haiphung1501@users.noreply.github.com>
2021-04-07 14:03:03 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -58,9 +58,9 @@ int** dealingForHand(int deck[SUITS][FACES])
 	//Tạo mảng 2 chiều = array
 	int temp[MAX_CARDS] = {0};
 	int t = 0;
-	int **arr = new int *[5];
+	int **result = new int *[5];
 	for ( int i = 0; i < 5; i++)
-		arr[i] = new int[2];
+		result[i] = new int[2];
 
 	for ( int i = 0; i < SUITS; i++)
 	{
@@ -73,36 +73,148 @@ int** dealingForHand(int deck[SUITS][FACES])
 	t = 0;
 	for ( int i = 0; i < 5; i++)
 	{
-			arr[i][0] = temp[t] / 4;
+			result[i][0] = temp[t] / 4;
 			t++;
 	}
 	t = 0;
 	for ( int i = 0; i < 5; i++)
 	{
-		arr[i][1] = temp[t] % 4;
+		result[i][1] = temp[t] % 4;
 		t++;
 	}
-	return arr;
+	//Sorting
+	int test = 0;
+	int test_1 = 0;
+	for ( int i = 0;  i < 4 ; i++){
+		for ( int j = i + 1; j < 5; j++)
+		{
+			if ( result[i][0] > result[j][0])
+			{
+				test = result[i][0];
+				result[i][0] = result[j][0];
+				result[j][0] = test;
+
+				test_1 = result[i][1];
+				result[i][1] = result[j][1];
+				result[j][1] = test_1;
+			}
+		}
+	}
+	return result;
 }
 void printHand(int** hand, char* suits[], char* faces[])
 {
 	for ( int i = 0; i < 5; i++)
-	{
-		cout << faces[hand[i][0]];
-		cout << suits[hand[i][1]];
-		cout << "\n";
-	}
+		cout << faces[hand[i][0]] << suits[hand[i][1]] << "\n";
 }	
-//int isFourOfAKind(int **hand)
 
+int isFourOfAKind(int** hand) {
+    int count = 1;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5;j++) {
+            if (hand[i][1] == hand[j][1]) {
+                count++;
+            }
+        }
+        if (count == 4) {
+            return 1;
+        }
+        else {
+            count = 1;
+        }
+    }
+    return 0;
+}
+int isFlush(int** hand) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5;j++) {
+            if (hand[i][0] != hand[j][0]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+int isThreeOfAKind(int** hand) {
+    int count = 1;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5;j++) {
+            if (hand[i][1] == hand[j][1]) {
+                count++;
+            }
+        }
+        if (count == 3) {
+            return 1;
+        }
+        else {
+            count = 1;
+        }
+    }
+    return 0;
+}
+int isStraight(int** hand) {
+    int* temp = new int[5];
+    for (int i = 0; i < 5; i++) {
+        temp[i] = hand[i][1];
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (temp[i] > temp[j]) {
+                int t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+        }
+    }
 
+    if (temp[0] == 0 && temp[1] == 9) { //xet truong hop 10 J Q K A
+        for (int i = 1; i < 4; i++) {
+            if (temp[i] != temp[i+1] - 1) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else { //truong hop binh thuong
+        for (int i = 0; i < 4; i++) {
+            if (temp[i] != temp[i+1] - 1) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+int isPair(int** hand)
+{
+    int count = 1;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5;j++) {
+            if (hand[i][1] == hand[j][1]) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            return 1;
+        }
+        else {
+            count = 1;
+        }
+    }
+}
+int isTwoPairs(int** hand){
+	
+}
 int main()
 {
 	int deck[4][13] = {0};
 	shuffleCards(deck);
-	printCardsShuffling(deck,suits,faces);
-	//dealingForHand(deck);
-	//printHand(dealingForHand(deck),suits,faces);
+	//printCardsShuffling(deck,suits,faces);
+	dealingForHand(deck);
+	//sortingCards(dealingForHand(deck));
+	printHand(dealingForHand(deck),suits,faces);
+	//if (isFourOfAKind(dealingForHand(deck)) == 1)
+		//cout << "Tu quy";
 	return 0;	
+
 }
 
